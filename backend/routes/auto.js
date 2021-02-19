85706969f58e48fe2534a99e3b4331b42350d27c
yarojo8327 <yeisonandresrojo@gmail.com>
2021-02-18 19:28:52 -0500

    Creacion de proyecto Front
    Creacion de proyecto Front

--- backend/routes/auto.js
+++ backend/routes/auto.js
@@ -15,8 +15,38 @@ router.post('/', async (req, res) => {
         precio: req.precio
     });
 
-    const resultado = await Auto.save();
+    const resultado = await auto.save();
     res.status(200).send(resultado);
+});
+
+router.put('/', async(req, res)=>{
+    const auto = await Auto.findByIdAndUpdate(
+        req.body._id,
+        {
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            color: req.body.color,
+            precio: req.body.precio
+        },
+        {
+            new : true
+        }
+    )
+
+    if(!auto){
+        res.status(400).send("No hay auto en la BD");
+    }
+    res.status(200).send(auto);
+});
+
+router.delete('/:_id', async(req, res)=>{
+    //const auto = await Auto.findById(req.params._id);
+
+    const auto = await Auto.findByIdAndDelete(req.params._id);   
+    if(!auto){
+        res.status(400).send('no hay auto en la BD');
+    }
+    res.status(200).send('Auto Eliminado');
 });
 
 module.exports = router;
